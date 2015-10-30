20677a7e0579efb24dae853eaabdb557128cece8
Ken Pierce <kpierce8@gmail.com>
2015-10-30 15:17:16 -0700

    trying chapter 5 section 1, esri widget having issues, moving to custom

--- app/js/controllers/appcontroller.js
+++ app/js/controllers/appcontroller.js
@@ -1,12 +1,40 @@
 define([
+	'dojo/_base/array',
 	'controllers/mapcontroller',
-	'widgets/edit/editTools'
-	], function(MapController, EditTools) {
+	'widgets/edit/editTools',
+	'esri/toolbars/edit',
+	'esri/dijit/editing/Editor',
+	'esri/dijit/editing/TemplatePicker',
+	'esri/config',
+	'esri/IdentityManager'
+	], function(array, MapController, EditTools, Edit, Editor, TemplatePicker, esriConfig) {
 		function mapLoaded(map) {
-			var editTools = new EditTools({
-				map:map
-			}, 'map-tools');
-			console.debug('map has been loaded ', map);
+			var requestLayer, layers = [], templatePicker;
+			requestLayer = map.getLayer('Requests');
+			layers.push(requestLayer);
+			templatePicker = new TemplatePicker({
+				featureLayers: layers,
+				rows: 'auto',
+				columns: 1
+			}, 'template-div');
+			templatePicker.startup();
+
+			var layerInfos = array.map(layers, function(layer) {
+				return {
+					featureLayer: layer
+				};
+			});
+			var settings = {
+				map:map,
+				templatePicker: templatePicker,
+				layerInfos: layerInfos
+			};
+			var params = {settings: settings};
+			var editorWidget = new Editor(params);
+			// var editTools = new EditTools({
+			// 	map:map
+			// }, 'map-tools');
+			// console.debug('map has been loaded ', map);
 		}
 		function init(config){
 			var mapCtrl = new MapController(config);
--- app/js/services/mapservices.js
+++ app/js/services/mapservices.js
@@ -3,24 +3,35 @@ define([
 	'esri/renderers/SimpleRenderer',
 	'utils/symbolUtil']
 	, function(FeatureLayer, SimpleRenderer, symbolUtil) {
-		function loadServices(){
-			var layers = [];
-			var censusLayer;
-			var renderer;
-			censusLayer = new FeatureLayer(
-				'http://services.arcgis.com/V6ZHFr6zdgNZuVG0/' +
-				'arcgis/rest/services/' +
-				'CensusLaborDemo/FeatureServer/1'
-			);
+		function _loadServices(config){
 
-	renderer = new SimpleRenderer(symbolUtil.renderSymbol());
-	censusLayer.setRenderer(renderer);
+			var CENSUS_URL = 'http://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/CensusLaborDemo/FeatureServer/1'
+			var REQUEST_URL = 'http://services.arcgis.com/rcya3vExsaVBGUDp/arcgis/rest/services/testTrees/FeatureServer/0'
+
+			
+
+			var layers = [];
+			var censusLayer = new FeatureLayer(CENSUS_URL, {
+				id: 'Census'
+			});
+			var	requestLayer = new FeatureLayer(REQUEST_URL, {
+				id: 'Requests',
+				mode: FeatureLayer.MODE_ONDEMAND,
+				outFields: ['*']
+			});
+
+			var renderer = new SimpleRenderer(symbolUtil.renderSymbol());
+		censusLayer.setRenderer(renderer);
 
 	layers.push(censusLayer);
+	layers.push(requestLayer);
 	return layers;
 	}
 	return {
-		loadServices: loadServices
+		loadServices: _loadServices
 		};
 
 	});
+
+
+// 
